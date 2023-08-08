ff1dc6472b9e02ef220c410d6451b38c36f414ba
Will Adams <bushblade@gmail.com>
2023-08-08 12:03:31 +0100

    fix: kajabi setting playback speed to 1 on first play

--- setSpeed.js
+++ setSpeed.js
@@ -5,6 +5,12 @@ function speedSettingClickEvent(indx) {
   }
 }
 
+function getSpeedSettingCheckBoxes() {
+  /** @type {NodeListOf<HTMLInputElement>} */
+  const speedSettingCheckBoxes = document.querySelectorAll('input[name=Speed]')
+  return speedSettingCheckBoxes
+}
+
 /** @param {NodeListOf<HTMLInputElement>} speedSettingCheckBoxes*/
 function addSpeedSettingsEventListeners(speedSettingCheckBoxes) {
   // add event listeners
@@ -21,31 +27,40 @@ function restoreSpeed(speedSettingCheckBoxes) {
   }
 }
 
+function getSettingsButton() {
+  /** @type {HTMLButtonElement} */
+  const settingsButton = document.querySelector(
+    'button[aria-label="Show settings menu"]'
+  )
+  return settingsButton
+}
+
+function isSettingsOpen() {
+  return getSettingsButton().getAttribute('aria-expanded') === 'true'
+}
+
 // NOTE: video settings are dynamically inserted into DOM after wista loads the
 // video.
 
 // only set the speed once
 let canSetSpeed = true
-let settingsOpened = false
+let settingsOpenedFirstTime = false
+// need to set again if first play to fix issue with Kajabi
+let firstPlay = true
 
 /** @param {Array<MutationRecord>} mutationList*/
 function mutationCallback(mutationList) {
   mutationList.forEach(() => {
     // need to open and close the settings to apply
-    /** @type {HTMLButtonElement} */
-    const settingsButton = document.querySelector(
-      'button[aria-label="Show settings menu"]'
-    )
+    const settingsButton = getSettingsButton()
 
-    if (settingsButton && !settingsOpened) {
+    if (settingsButton && !settingsOpenedFirstTime) {
       // open settings
       settingsButton.click()
-      settingsOpened = true
+      settingsOpenedFirstTime = true
     }
 
-    /** @type {NodeListOf<HTMLInputElement>} */
-    const speedSettingCheckBoxes =
-      document.querySelectorAll('input[name=Speed]')
+    const speedSettingCheckBoxes = getSpeedSettingCheckBoxes()
 
     // check that the speed settings are there and we can set speed
     if (
@@ -57,7 +72,12 @@ function mutationCallback(mutationList) {
       restoreSpeed(speedSettingCheckBoxes)
       canSetSpeed = false
       // close settings
-      if (settingsOpened) settingsButton.click()
+      if (isSettingsOpen()) settingsButton.click()
+    }
+
+    const video = document.querySelector('video')
+    if (video && settingsButton) {
+      watchForFirstPlay(video)
     }
   })
 }
@@ -70,3 +90,22 @@ const observerOptions = {
 const mutationObserver = new MutationObserver(mutationCallback)
 
 mutationObserver.observe(videoWrapper, observerOptions)
+
+// FIX: Kajabi resets playback speed to 1 on first play of video
+/**
+ * @param {HTMLVideoElement} video
+ */
+function watchForFirstPlay(video) {
+  const handlePlayback = () => {
+    if (firstPlay) {
+      restoreSpeed(getSpeedSettingCheckBoxes())
+      firstPlay = false
+      // close settings if open
+      if (isSettingsOpen()) {
+        getSettingsButton().click()
+      }
+    }
+  }
+
+  video.addEventListener('playing', handlePlayback)
+}
