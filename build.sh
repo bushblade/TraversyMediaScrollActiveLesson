a93e4b9b53022d2ce71cbdb9897b697b6e49e28f
Will Adams <bushblade@gmail.com>
2023-08-09 18:15:21 +0100

    ignore git repo when zipping

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 
 # Copy Firefox manifest and create Firefox extension zip
 cp manifest.firefox.json manifest.json
-zip -r firefox-extension.zip . -x "*.git*" "firefox-extension.zip" "chrome-extension.zip" "*.DS_Store*"
+zip -r firefox-extension.zip . -x "*.git/*" "firefox-extension.zip" "chrome-extension.zip"
 echo "Firefox extension zipped as firefox-extension.zip"
 
 # Copy Chrome manifest and create Chrome extension zip
 cp manifest.chrome.json manifest.json
-zip -r chrome-extension.zip . -x "*.git*" "firefox-extension.zip" "chrome-extension.zip" "*.DS_Store*"
+zip -r chrome-extension.zip . -x "*.git/*" "firefox-extension.zip" "chrome-extension.zip"
 echo "Chrome extension zipped as chrome-extension.zip"
 
 # Remove temporary manifest.json file
