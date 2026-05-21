f5b828b4d2777b1422443ada6e6e092e1288d1cb
will <bushblade@gmail.com>
2026-05-21 13:08:59 +0100

    ignore ts warning for chrome runtime

--- videoSettings.js
+++ videoSettings.js
@@ -1,6 +1,7 @@
 const script = document.createElement('script');
 
 // Securely fetch the file from the extension's local directory
+// @ts-ignore
 script.src = chrome.runtime.getURL('wistia-injector.js');
 
 // Clean up the script tag from the DOM after it successfully loads and executes
