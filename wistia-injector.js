746bb906f80169375809f15fc0b59db984520332
will <bushblade@gmail.com>
2026-05-21 16:41:30 +0100

    formatting and linting with biome

--- wistia-injector.js
+++ wistia-injector.js
@@ -1,28 +1,27 @@
 // Use bracket notation to satisfy the linter for dynamic window properties
-window['_wq'] = window['_wq'] || [];
+window['_wq'] = window['_wq'] || []
 
 window['_wq'].push({
-  id: '_all', 
-  onReady: function (/** @type {any} */ video) {
-    
+  id: '_all',
+  onReady: (/** @type {any} */ video) => {
     const applySpeed = () => {
-      const savedSpeed = localStorage.getItem('tm-speed');
+      const savedSpeed = localStorage.getItem('tm-speed')
       if (savedSpeed) {
-        video.playbackRate(parseFloat(savedSpeed));
+        video.playbackRate(parseFloat(savedSpeed))
       }
-    };
+    }
 
     // 1. Apply immediately when the player loads
-    applySpeed();
+    applySpeed()
 
     // 2. Save the speed if the user manually clicks the gear icon
-    video.bind('playbackratechange', function (/** @type {number} */ speed) {
-      localStorage.setItem('tm-speed', speed.toString());
-    });
+    video.bind('playbackratechange', (/** @type {number} */ speed) => {
+      localStorage.setItem('tm-speed', speed.toString())
+    })
 
     // 3. Override Kajabi resetting the speed to 1x when the user hits play
-    video.bind('play', function () {
-      setTimeout(applySpeed, 100);
-    });
-  }
-});
+    video.bind('play', () => {
+      setTimeout(applySpeed, 100)
+    })
+  },
+})
